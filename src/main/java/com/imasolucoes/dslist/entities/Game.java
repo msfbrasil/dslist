42fc61566d21ec9bbf306d943e3b6cf2c8bc1181
Mauro Sergio Ferreira Brasil <msfbrasil@gmail.com>
2023-07-11 19:13:17 -0300

    Properties, Game entity and ORM.

---
+++ src/main/java/com/imasolucoes/dslist/entities/Game.java
@@ -0,0 +1,170 @@
+package com.imasolucoes.dslist.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table( name = "tb_game" )
+public class Game {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    private String title;
+
+    @Column( name = "game_year" )
+    private Integer year;
+
+    private String gender;
+
+    private String platform;
+
+    private String imgUrl;
+
+    private String shortDescription;
+
+    private String longDescription;
+
+    // --
+
+    public Game() {
+
+    }
+
+    // --
+
+    public Game(
+        Long id,
+        String title,
+        Integer year,
+        String gender,
+        String platform,
+        String imgUrl,
+        String shortDescription,
+        String longDescription ) {
+
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.gender = gender;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    // --
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setId( Long id ) {
+
+        this.id = id;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle( String title ) {
+
+        this.title = title;
+    }
+
+    public Integer getYear() {
+
+        return year;
+    }
+
+    public void setYear( Integer year ) {
+
+        this.year = year;
+    }
+
+    public String getGender() {
+
+        return gender;
+    }
+
+    public void setGender( String gender ) {
+
+        this.gender = gender;
+    }
+
+    public String getPlatform() {
+
+        return platform;
+    }
+
+    public void setPlatform( String platform ) {
+
+        this.platform = platform;
+    }
+
+    public String getImgUrl() {
+
+        return imgUrl;
+    }
+
+    public void setImgUrl( String imgUrl ) {
+
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+
+        return shortDescription;
+    }
+
+    public void setShortDescription( String shortDescription ) {
+
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+
+        return longDescription;
+    }
+
+    public void setLongDescription( String longDescription ) {
+
+        this.longDescription = longDescription;
+    }
+
+    // --
+
+    @Override
+    public int hashCode() {
+
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+
+        if ( this == obj ) {
+
+            return true;
+        }
+
+        if ( ( obj == null ) || ( getClass() != obj.getClass() ) ) {
+
+            return false;
+        }
+
+        Game game = ( Game ) obj;
+
+        return id.equals( game.id );
+    }
+}
