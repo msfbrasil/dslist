be943ad242e80745c61c5fb9be122cff8a867919
Mauro Sergio Ferreira Brasil <msfbrasil@gmail.com>
2023-07-11 21:44:32 -0300

    New REST service 'findById'.

--- src/main/java/com/imasolucoes/dslist/controllers/GameController.java
+++ src/main/java/com/imasolucoes/dslist/controllers/GameController.java
@@ -1,10 +1,13 @@
 package com.imasolucoes.dslist.controllers;
 
 
+import com.imasolucoes.dslist.dto.GameDTO;
 import com.imasolucoes.dslist.dto.GameMinDTO;
 import com.imasolucoes.dslist.services.GameService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping( value = "/{id}" )
+    public GameDTO findById( @PathVariable Long id ) {
+
+        return this.gameService.findById( id );
+    }
 
     @GetMapping
     public List< GameMinDTO > findAll() {
--- src/main/java/com/imasolucoes/dslist/services/GameService.java
+++ src/main/java/com/imasolucoes/dslist/services/GameService.java
@@ -1,12 +1,14 @@
 package com.imasolucoes.dslist.services;
 
 
+import com.imasolucoes.dslist.dto.GameDTO;
 import com.imasolucoes.dslist.dto.GameMinDTO;
 import com.imasolucoes.dslist.entities.Game;
 import com.imasolucoes.dslist.repositories.GameRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +20,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional( readOnly = true )
+    public GameDTO findById( Long id ) {
+
+        return new GameDTO( gameRepository.findById( id ).get() );
+    }
+
+    @Transactional( readOnly = true )
     public List< GameMinDTO > findAll() {
 
         List< Game > gameList = this.gameRepository.findAll();
